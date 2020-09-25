a76b144b29fc8956b8de84056b6ec4ea595e6058
Sujin Park <q9q989@gmail.com>
2020-09-25 11:14:10 +0900

    Add urls and templates

--- restaurantshare/shareRes/views.py
+++ restaurantshare/shareRes/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    # return HttpResponse('index')
+    return render(request, 'index.html')
+
+
+def restaurantDetail(request):
+    # return HttpResponse('restaurant detail')
+    return render(request, 'restaurantDetail.html')
+
+def restaurantCreate(request):
+    # return HttpResponse('restaurant create')
+    return render(request, 'restaurantCreate.html')
+
+def categoryCreate(request):
+    # return HttpResponse('category create')
+    return render(request, 'categoryCreate.html')
